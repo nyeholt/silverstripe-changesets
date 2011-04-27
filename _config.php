681039485f2a87b4a508a5b5c85f77ba567ba476
Marcus Nyeholt <marcus@silverstripe.com.au>
2011-04-27 12:50:46 +1000

    Move to using the publishable objects module

--- _config.php
+++ _config.php
@@ -29,3 +29,7 @@ define('CHANGESETS_DIR', 'changesets');
 
 // Add the following to your _config to enable this module
 // DataObject::add_extension('SiteTree', 'ChangesetTrackable');
+
+if (!class_exists('PublishableObject')) {
+	throw new Exception('The changesets module requires the publishableobjects module (http://github.com/nyeholt/silverstripe-publishableobjects) ');
+}
